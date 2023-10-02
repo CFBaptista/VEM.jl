7997af440e9b5a8632e48c9acbee7504ddeb5d9b
Documenter.jl <documenter@juliadocs.github.io>
2023-10-02 01:03:59 +0000

    build based on 027cc4d

---
+++ previews/PR1/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#VEM.jl","page":"Home","title":"VEM.jl","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"VEM (Vortex Element Method) is a library for computational fluid dynamics based on the evolution of vorticity. The fluid dynamics are governed by the Navier-Stokes equation in its velocity-vorticity formulation. Circulation-carrying computational elements called vortex elements induce a vorticity field which represents the entire flow field. An increasing amount of approaches exist for evolving the vortex elements in time. This library is meant for collecting all approaches in a single package.","category":"page"}]
+}
