bbc4f986d68b1a11f398fbd522a97569623d88c6
Carlos Fernando Baptista <cfd.baptista@gmail.com>
2025-10-11 00:51:53 +0200

    Simplify default Float definition

--- src/definitions.jl
+++ src/definitions.jl
@@ -1,34 +1,6 @@
 """
-    float_type([bit_size::Union{Nothing, Int}=nothing])
+    Float
 
-Returns the floating-point type (`Float32` or `Float64`) corresponding to the given `bit_size`.
-If `bit_size` is not provided or is `nothing`, the function uses the system's word size to determine the type.
-
-# Arguments
-- `bit_size::Union{Nothing, Int}`: Optional. The bit size (32 or 64) to select the floating-point type. If `nothing`, uses `Sys.WORD_SIZE`.
-
-# Returns
-- `Float32` or `Float64` depending on the provided `bit_size` or system word size if `bit_size` is `nothing`.
-
-# Throws
-- `ArgumentError`: If an unsupported `bit_size` is provided.
+Default floating-point type (`Float32` or `Float64`) corresponding to the system's word size.
 """
-function float_type(bit_size::Union{Nothing,Int}=nothing)
-    if bit_size === nothing
-        word_size = Sys.WORD_SIZE
-    else
-        word_size = bit_size
-    end
-
-    if word_size == 32
-        Float = Float32
-    elseif word_size == 64
-        Float = Float64
-    else
-        throw(ArgumentError("Unsupported word size: $(word_size)"))
-    end
-
-    return Float
-end
-
-const Float = float_type()
+const Float = Sys.WORD_SIZE == 32 ? Float32 : Float64
--- test/definitions.jl
+++ test/definitions.jl
@@ -5,49 +5,7 @@
 
     # WHEN
 
-    float_type = VEM.float_type()
-
     # THEN
 
-    @test float_type == expected_float_type
-end
-
-@testitem "32 bit size returns Float32" begin
-    # GIVEN
-
-    bit_size = 32
-    expected_float_type = Float32
-
-    # WHEN
-
-    float_type = VEM.float_type(bit_size)
-
-    # THEN
-
-    @test float_type == expected_float_type
-end
-
-@testitem "64 bit size returns Float64" begin
-    # GIVEN
-
-    bit_size = 64
-    expected_float_type = Float64
-
-    # WHEN
-
-    float_type = VEM.float_type(bit_size)
-
-    # THEN
-
-    @test float_type == expected_float_type
-end
-
-@testitem "128 bit size throws error" begin
-    # GIVEN
-
-    bit_size = 128
-
-    # WHEN / THEN
-
-    @test_throws ArgumentError VEM.float_type(bit_size)
+    @test Float == expected_float_type
 end
